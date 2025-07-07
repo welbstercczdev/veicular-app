67bef2424e8539111c878719acb8f19944943a6b
welbstercczdev <welbsterccz@gmail.com>
2025-07-07 14:32:35 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,12 +6,17 @@ const urlsToCache = [
   '/app.js',
   '/quadras.geojson',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css',
+  'https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css',
+  'https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js',
+  'https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
+      console.log('Cache aberto');
       return cache.addAll(urlsToCache);
     })
   );
@@ -20,6 +25,7 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
+      // Se encontrar no cache, retorna. Senão, busca na rede.
       return response || fetch(event.request);
     })
   );
